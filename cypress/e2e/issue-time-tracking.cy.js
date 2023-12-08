5742381d60391472e49930323a76311110bc3f6a
mmeest <martin.maasik.001@mail.ee>
2023-12-08 22:09:19 +0200

    Updated tests

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -3,25 +3,108 @@ describe('Issue time tracking, editing and removing', () => {
         cy.visit('/');
         cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
             cy.visit(url + '/board');
-            cy.contains('This is an issue of type: Task').click();
+            cy.contains('This is an issue of type: Task').click();                    
         });
     });
 
-    const getEstimatedTimeInput = () => cy.contains('div', 'Original Estimate (hours)').next('div').find('input');
-    const getTimeTrackingModal = () => cy.get('[data-testid="icon:stopwatch"').click();
-    const getTimeSpentInput = () => cy.contains('div', 'Time spent (hours)').next('div').find('input');
 
+    // Modal Detail Page
+    const openIssueDetailPage = () => cy.contains('This is an issue of type: Task').click();
+    const closeIssueDetailPage = () => cy.get('[data-testid="icon:close"]').first().click();
+
+    // Time Estimation
+    const getEstimatedTimeInput = () => cy.contains('div', 'Original Estimate (hours)').next('div').find('input');
     const addValueToEstimatedTime = timeValue => getEstimatedTimeInput().clear().type(`${timeValue}`);
     const clearValueOnEstimatedTime = () => getEstimatedTimeInput().clear();
+    
+    // Time Logging
+    const getTimeTrackingModal = () => cy.get('[data-testid="icon:stopwatch"').click();
+    const getTimeSpentInput = () => cy.contains('div', 'Time spent (hours)').next('div').find('input');
+    const getTimeRemainingInput = () => cy.contains('div', 'Time remaining (hours)').next('div').find('input');
+    const changeValueOnTimeSpent = spentHours => getTimeSpentInput().should('exist').clear().type(spentHours);
+    const changeValueOnTimeRemaining = remainingHours => getTimeRemainingInput().should('exist').clear().type(remainingHours);
+    const clearValueOnTimeSpent = () => getTimeSpentInput().clear();
+    const clearValueOnTimeRemaining = () => getTimeRemainingInput().clear();
+    const clickDoneButton = () => cy.contains('button', 'Done').click();
+    
+    // Assertions
     const assertRightValueOnEstimatedTime = timeValue => getEstimatedTimeInput().should('exist').should('have.value', timeValue);
     const assertEstimatedTimeToBeEmpty = () => getEstimatedTimeInput().should('exist').should('have.value', '');
-
-    const changeValueOnTimeSpent = spentHours => getTimeSpentInput().should('exist').clear().type(spentHours);
-    const clearValueOnTimeSpent = () => getTimeSpentInput().clear();
     const assertRightValueOnTimeSpent = spentHours => getTimeSpentInput().should('exist').should('have.value', spentHours);
     const assertEmptyTimeSpent = () => getTimeSpentInput().should('exist').should('have.value', '');
+    const assertTimeTrackingModalIsVisible = () => cy.contains('div', 'Time tracking').should('be.visible');
+    const assertNoTimeLogged = () => cy.contains('div', 'No time logged').should('be.visible');
+    const assertTimeLogged = () => cy.contains('div', 'No time logged').should('not.exist');
+    const assertHoursLogged = loggedHours => cy.get('[data-testid="modal:issue-details"]')
+        .find('div', `${loggedHours} logged`).should('exist').should('be.visible');
+    const assertHoursRemaining = remainingHours => cy.get('[data-testid="modal:issue-details"]')
+        .find('div', `${remainingHours} remaining`).should('exist').should('be.visible');
 
-    it('Should add, edit and clear time estimation', () => {
+
+    it('Should add estimation', () => {
+        const myEstimation = 10;
+
+        assertTimeLogged();
+        addValueToEstimatedTime(myEstimation);
+
+        cy.wait(2000).then(() => {
+            closeIssueDetailPage();
+            openIssueDetailPage();
+            assertRightValueOnEstimatedTime(myEstimation);
+        });
+    });
+
+    it('Should update estimation', () => {
+        const newEstimatedHours = 20;
+
+        addValueToEstimatedTime(newEstimatedHours);
+
+        cy.wait(2000).then(() => {
+            closeIssueDetailPage().then(() => {
+                openIssueDetailPage();
+                assertRightValueOnEstimatedTime(newEstimatedHours);
+            });
+        });
+    });
+
+    it('Should remove time estimation', () => {
+
+        clearValueOnEstimatedTime();
+
+        cy.wait(2000).then(() => {
+            closeIssueDetailPage().then(() => {
+                openIssueDetailPage();
+                assertEstimatedTimeToBeEmpty();
+            })
+        });
+    });
+
+    it('Should log time', () => {
+        const timeSpent = 2;
+        const timeRemaining = 5;
+
+        getTimeTrackingModal();
+        assertTimeTrackingModalIsVisible();
+        changeValueOnTimeSpent(timeSpent);
+        changeValueOnTimeRemaining(timeRemaining);
+        assertTimeLogged();
+        clickDoneButton();
+        assertHoursLogged(timeSpent);
+        assertHoursRemaining(timeRemaining);
+    });
+
+    it('Should remove logged time', () => {
+        getTimeTrackingModal();
+        assertTimeTrackingModalIsVisible();
+        clearValueOnTimeSpent();
+        clearValueOnTimeRemaining();
+        clickDoneButton();
+        assertNoTimeLogged();
+    });
+
+    // ------------------B-O-N-U-S-----------------
+
+    /* it('Should add, edit and clear time estimation', () => {
         const originalValue = 8;
         const estimationFirst = 12;
         const estimationSecond = 0;
@@ -54,5 +137,5 @@ describe('Issue time tracking, editing and removing', () => {
 
         clearValueOnTimeSpent();
         assertEmptyTimeSpent();
-    });
+    }); */
 });
